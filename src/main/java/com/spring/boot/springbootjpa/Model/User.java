ca84fcbde29efea974f37f7e5c23098fc4a89867
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-08-04 09:00:11 +0300

    default values fix

--- src/main/java/com/spring/boot/springbootjpa/Model/User.java
+++ src/main/java/com/spring/boot/springbootjpa/Model/User.java
@@ -44,9 +44,9 @@ public class User {
     private Double balance;
 
     // TODO Extra user variables:
-    @Column(columnDefinition = "varchar(30) default ''")
+    @Column(columnDefinition = "varchar(30) not null default ''")
     private String orderHistory;
 
-    @Column(columnDefinition = "varchar(30) default ''")
+    @Column(columnDefinition = "varchar(30) not null default ''")
     private String favoriteCategory;
 }
